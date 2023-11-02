939891d837b895ea12c7cb459e94e84af48254fb
Philex007 <philemon.tamrat@gmail.com>
2023-11-02 06:13:42 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include < stdio.h>
-#include < stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
@@ -46,15 +46,15 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
- * @argc: THe number of arguments supplied to the program.
+ * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exitcode 100.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
@@ -101,5 +101,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
-
