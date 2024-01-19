87c5ae5b26054532c1ecfe97406ae1f731095f55
Philex007 <philemon.tamrat@gmail.com>
2024-01-19 12:26:36 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-wget -P /tmp https://github.com/Philex007/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+[200~#!/bin/bash
+cp ../sh2.sh .
+. ./sh2.sh
